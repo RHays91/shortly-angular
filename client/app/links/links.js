815b79dbf8766d772cc7fc3da38b9c06e82bb792
RHays91 <rhaysvm42@gmail.com>
2015-07-31 15:45:01 -0700

    Passed Links Tests

--- client/app/links/links.js
+++ client/app/links/links.js
@@ -2,4 +2,11 @@ angular.module('shortly.links', [])
 
 .controller('LinksController', function ($scope, Links) {
   // Your code here
+  $scope.data = Links;
+  $scope.getLinks = Links.getLinks;
+
+  Links.getLinks().then(function(links){
+    $scope.data.links = links;
+  });
+  
 });
